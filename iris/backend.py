260774cecb7e5ca1f920661d89656bae45e02623
jmoiron <jmoiron@jmoiron.net>
2010-10-28 02:47:41 -0400

    start feeling around a bit more on how to add data to the database

--- iris/backend.py
+++ iris/backend.py
@@ -5,8 +5,15 @@
 
 Iris uses mongodb because it's web scale."""
 
+import os
+import imghdr
+
 import pymongo
 
+from iris.loaders import file, picasa
+from iris.utils import memoize
+
+@memoize
 def get_database(host=None, port=None):
     """Get the iris mongo db from host and port.  If none are supplied, attempt
     to read an iris configuration file.  If it doesn't exist, connect to
@@ -20,4 +27,40 @@ def get_database(host=None, port=None):
         except:
             host, port = 'localhost', 27017
     connection = pymongo.Connection(host, port)
+    db = connection.iris
+    photos = db.photos
+    photos.create_index([('path', pymongo.DESCENDING)])
+    photos.create_index([('date', pymongo.DESCENDING)])
     return connection.iris
+
+class Photo(object):
+    def __init__(self, path_or_dict):
+        if isinstance(path_or_dict, basestring):
+            self.load(path_or_dict)
+        else:
+            self._init_from_dict(path_or_dict)
+
+    def load(self, path):
+        path = os.path.realpath(path)
+        meta = file.MetaData(path)
+        self.exif = meta.exif()
+        self.iptc = meta.iptc()
+        self.path = path
+        stat = os.stat(path)
+        self.size = stat.st_size
+
+    def _init_from_dict(self, d):
+        """When loading from the db."""
+        self.__dict__.update(d)
+
+    def _save(self):
+        db = get_database()
+        if not self._id:
+            photo = db.photos.find_one({'path': self.path})
+            if photo: self._id = photo['_id']
+        value = dict(self.__dict__)
+        db.photos.save(value)
+
+class FileLoader(object):
+    pass
+
--- iris/script.py
+++ iris/script.py
@@ -3,7 +3,10 @@
 
 """Support for the iris script."""
 
+import os
+
 from cmdparse import Command, CommandParser
+from iris import backend
 
 class AddCommand(Command):
     """Add a photo or directory of photos."""
@@ -12,7 +15,11 @@ class AddCommand(Command):
         self.add_option('-r', '--recursive', action='store_true', default=False)
 
     def run(self, options, args):
-        print "add: ", options, args
+        """Args here are a bunch of file or directory names.  We want to
+        mostly defer to other functions that do the stuff for us."""
+        for arg in args:
+            # TODO: handle error conditions here
+            backend.load_path(arg, recursive=options['recursive'])
 
 class TagCommand(Command):
     """Tag one or more photos.
@@ -26,9 +33,18 @@ class TagCommand(Command):
     def __init__(self):
         Command.__init__(self, "tag", summary="tag photos by filename, query, etc.")
         self.add_option('-r', '--recursive', action='store_true', default=False)
+        self.add_option('-q', '--query', action='store_true', default=False, help="query instead of paths")
 
     def run(self, options, args):
         print "tag: ", options, args
+
+class QueryCommand(Command):
+    """Query iris' database for photos."""
+    def __init__(self):
+        Command.__init__(self, "query", summary="query for photos")
+    
+    def run(self, options, args):
+        print "query: ", options, args
 
 class HelpCommand(Command):
     """Provides extended help for other commands."""
@@ -49,6 +65,7 @@ def main():
     parser.add_command(HelpCommand())
     parser.add_command(AddCommand())
     parser.add_command(TagCommand())
+    parser.add_command(QueryCommand())
     command, options, args = parser.parse_args()
     if command is None:
         parser.print_help()
