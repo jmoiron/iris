15b4dc636d6dbb51787ed3f45286a54afd7d8f7c
Jason Moiron <jmoiron@jmoiron.net>
2010-10-31 18:45:56 -0400

    add new BulkUpdater backend class, which creates an updater that will intelligently mass-insert, to a threshold of 100 documents.  even if they are all truly updates, it still saves N reads (N=threshold) per flush as normally these documents would have to be checked for existence in the collection manually by their unique value, but here it's done in one query

--- iris/backend.py
+++ iris/backend.py
@@ -34,43 +34,48 @@ def get_database(host=None, port=None):
     photos.create_index([('date', pymongo.DESCENDING)])
     return connection.iris
 
+
 class BulkUpdater(object):
     """A caching updater for mongo documents going into the same collection.
     You can choose a threshold, and add documents to it, and they will be
     flushed after the threshold number of documents have been reached."""
-    def __init__(self, collection, flush_threshold=100):
+    def __init__(self, collection, threshold=100, unique_attr=None):
         # this has to be reentrant so we can protect flushes
         self.collection = collection
-        self.lock = threading.RLock()
-        self.flush_threshold = 100
+        self.unique_attr = unique_attr
+        self.threshold = threshold
         self.total = 0
         self.documents = {
             'updates' : [],
             'inserts' : [],
         }
+        self._inserts = 0
+        self._updates = 0
+        self.lock = threading.RLock()
 
-    def update(self, document):
-        """Add a document to be updated whenever the threshold is met.  If the
-        document has an '_id', it's considered an update.  If it doesn't, it's
-        considered an insert.  If the document has a '_unique_attr' attribute,
-        it's used later to check if it is actually an insert or an update.
-        This method is thread safe."""
+    def update(self, *documents):
+        """Add one or more documents to be updated whenever the threshold is met.
+        If the document has an '_id', it's considered an update.  If it doesn't,
+        it's considered an insert.  If the document has a '_unique_attr'
+        attribute, it's used later to check if it is actually an insert or an
+        update.  This method is thread safe."""
         self.lock.acquire()
-        if '_id' in document:
-            self.documents['updates'].append(document)
-        else:
-            self.documnets['inserts'].append(document)
-        self.total += 1
-        if self.total >= self.total:
-            self.flush()
+        for document in documents:
+            if '_id' in document:
+                self.documents['updates'].append(document)
+            else:
+                self.documents['inserts'].append(document)
+            self.total += 1
+            if self.total >= self.threshold:
+                self.flush(False)
         self.lock.release()
 
-    def flush(self, force=False):
+    def flush(self, force=True):
         """Flush all of the documents with as few queries as possible.  If
         force is False (default), documents are only saved if they meet the
         threshold.  This method is thread safe."""
         self.lock.acquire()
-        if self.total < self.flush_threshold and not force:
+        if self.total < self.threshold and not force:
             self.lock.release()
             return
         self._flush()
@@ -82,12 +87,58 @@ class BulkUpdater(object):
         a '_unique_attr' attribute), and moves those that exist over to the
         'updates', then bulk inserts whatever's left and saves the others one
         at a time.  This method is NOT thread safe."""
-        updates, inserts = self.documents['updates'], self.documents['inserts']
+        updates = self.documents['updates']
+        inserts = self.documents['inserts']
         lookups = {}
+        # check all insert documents for a 'unique attr' that will allow us
+        # to search the db for possible duplicates
         for document in inserts:
-            unique = getattr(document, '_unique_attr', None)
+            unique = getattr(document, '_unique_attr', self.unique_attr)
             if unique:
-                lookups.setdefault(unique, []).append(document[unique])
+                lookups.setdefault(unique, {})[document[unique]] = document
+        # search db for duplicates based on unique attrs of insert documents
+        results = {}
+        for lookup,values in lookups.iteritems():
+            keys = [lookup]
+            if '_id' not in keys:
+                keys.append('_id')
+            spec = {lookup: {'$in' : list(values)}}
+            results[lookup] = dict([(d[lookup], d['_id']) for d in self.collection.find(spec, keys)])
+        # for each match, remove that document from inserts, add the _id from
+        # the database, and add that document to updates
+        for key in results:
+            for unique, _id in results[key].iteritems():
+                # add _id to document in inserts with unique value 'unique'
+                document = lookups[key][unique]
+                # XXX: this is O(n) but it could be O(1);  room for improvement
+                # here with potentially large docucment cache thresholds
+                inserts.remove(document)
+                document['_id'] = _id
+                updates.append(document)
+        if inserts:
+            self.collection.insert(inserts)
+        for doc in updates:
+            self.collection.save(doc)
+        self._clear()
+
+    def _clear(self):
+        """Clears out documents that have already been flushed.  This method
+        is NOT thread safe."""
+        self._inserts += len(self.documents['inserts'])
+        self._updates += len(self.documents['updates'])
+        self.documents.clear()
+        self.documents['updates'] = []
+        self.documents['inserts'] = []
+        self.total = 0
+
+
+class PagingCursor(object):
+    def __init__(self, collection, sort=None):
+        self.collection = collection
+        self.sort = sort or [('_id', pymongo.DESCENDING)]
+
+    def find(self, spec, fields=None, sort=None):
+        pass
 
 class Model(OpenStruct):
     def save(self):
---
+++ tests/backend.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""iris backend tests."""
+
+from unittest import TestCase
+from iris import backend
+
+class BulkUpdaterTest(TestCase):
+    def __init__(self, *args):
+        super(BulkUpdaterTest, self).__init__(*args)
+        self.db = backend.get_database()
+        self.collection = 'BulkUpdaterTest'
+
+    def tearDown(self):
+        self.db.drop_collection(self.collection)
+
+    def _collision_setup(self):
+        collection = self.db[self.collection]
+        documents = [{'value': str(i)} for i in xrange(1, 200+1, 2)]
+        collection.insert(documents)
+
+    def test_normal_insertion(self):
+        collection = self.db[self.collection]
+        documents = [{'value': str(i)} for i in xrange(1, 15+1)]
+        updater = backend.BulkUpdater(collection, threshold=10, unique_attr='value')
+        for doc in documents:
+            updater.update(doc)
+        # we should have inserted 10 documents, with 5 hanging around
+        self.assertEquals(collection.find().count(), 10)
+        self.assertEquals(updater.total, 5)
+        self.assertEquals(updater._inserts, 10)
+        updater.flush()
+        self.assertEquals(collection.find().count(), 15)
+        self.assertEquals(updater.total, 0)
+        self.assertEquals(updater._inserts, 15)
+        # this time, they should be updates, even though they 'look' like inserts
+        documents = [{'value': str(i), 'foo': 'bar'} for i in xrange(1, 15+1)]
+        for doc in documents:
+            updater.update(doc)
+        # here, we're updating documents with some changes to them; the inserts
+        # would fail silently if they were tried with documents that had _ids,
+        # and the collection would grow if these were not updates
+        self.assertEquals(collection.find({'foo': 'bar'}).count(), 10)
+        updater.flush()
+        self.assertEquals(collection.find({'foo': 'bar'}).count(), 15)
+        self.assertEquals(collection.find().count(), 15)
+        self.assertEquals(updater._inserts, 15)
+        self.assertEquals(updater._updates, 15)
+
+    def test_mixed_insertion(self):
+        collection = self.db[self.collection]
+        self._collision_setup()
+        documents = [{'value': str(i), 'foo': 'bar'} for i in xrange(1, 200+1)]
+        updater = backend.BulkUpdater(collection, threshold=50, unique_attr='value')
+        updater.update(*documents)
+        self.assertEquals(updater._updates, 100)
+        self.assertEquals(updater._inserts, 100)
+        self.assertEquals(collection.find({'foo':'bar'}).count(), 200)
+
+
+
+
