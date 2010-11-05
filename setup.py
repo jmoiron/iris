0c18c7962d694dcd3401e18e593bfb6058153d87
Jason Moiron <jmoiron@jmoiron.net>
2010-11-05 03:14:21 -0400

    update setup.py to have pymongo and lepl as dependencies

--- setup.py
+++ setup.py
@@ -35,9 +35,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     test_suite="tests",
-    install_requires=[
-      # -*- Extra requirements: -*-
-    ],
+    # -*- Extra requirements: -*-
+    install_requires=['lepl', 'pymongo',],
     entry_points="""
     # -*- Entry points: -*-
     """,
